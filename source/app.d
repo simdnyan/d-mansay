cbae1a18fc72c1d3e54bb2fcd78f4a042ab1324a
Yoshinori SHIMADA <simd.nyan@gmail.com>
2016-12-08 22:12:45 +0900

    first commit

---
+++ source/app.d
@@ -0,0 +1,135 @@
+import std.stdio,
+       std.string,
+       std.getopt,
+       std.algorithm.comparison;
+
+const string dman =
+`    t    _   _
+     t  (_) (_)
+       /______ \
+       \ (e(e \/
+        | | | |
+        | |_| |
+       /______/
+         <   >
+        (_) (_)
+`;
+const string[] eyes = ["=", "X", "$", "@", "*", "-", "O", "."];
+const string[] usage =[
+  `d-man{say,think} version 0.0.1, (c) 2016 @simd_nyan`,
+  `Usage: d-man{say,think} [-bdgpstwy] [-h] [-n] [-W wrapcolumn] [message]`
+];
+version (say)
+{
+  string thought = `\`;
+  const string[] baloons = [
+    "< ", "< ", "< ",  " >\n",  " >\n", " >\n",
+    "/ ", "| ", "\\ ", " \\\n", " |\n", " /\n"
+  ];
+}
+version(think)
+{
+  string thought = `o`;
+  const string[] baloons = [
+    "( ", "( ", "( ", " )\n", " )\n", " )\n",
+    "( ", "( ", "( ", " )\n", " )\n", " )\n"
+  ];
+}
+
+class Message
+{
+  string[] lines;
+  bool  wordwrap;
+  ulong wrapcolumn;
+  ulong max_length = 0;
+
+  this(bool wordwrap, ulong wrapcolumn)
+  {
+    this.wordwrap   = wordwrap;
+    this.wrapcolumn = wordwrap ? ulong.max : wrapcolumn;
+  }
+
+  void add(string line)
+  {
+    max_length = min(max(max_length, line.length), wrapcolumn);
+    for (int i = 0; i < line.length; i += max_length)
+      lines ~= line[i..min(i + wrapcolumn, line.length)];
+  }
+
+  void print()
+  {
+    write_line('_');
+    foreach(int n, string o; lines){
+      int b = ((lines.length > 1) ?  6 : 0) + (n > 0) + (n == lines.length - 1);
+      write(baloons[b]);
+      write(leftJustify(o, max_length));
+      write(baloons[b + 3]);
+    }
+    write_line('-');
+  }
+
+  void write_line(char c)
+  {
+    write(" ");
+    for(int i; i < max_length + 2; i++)
+      write(c);
+    writeln();
+  }
+
+}
+
+void main(string[] args){
+
+  bool   help       = false;
+  bool   wordwrap   = false;
+  ulong  wrapcolumn = 40;
+  bool[] eye_flags  = [false, false, false, false, false, false, false, false];
+  string eye        = "O";
+  getopt(args,
+    std.getopt.config.passThrough,
+    std.getopt.config.caseSensitive,
+    "h|help",  &help,
+    "n",  &wordwrap,
+    "W",  &wrapcolumn,
+    "b",  &eye_flags[0],
+    "d",  &eye_flags[1],
+    "g",  &eye_flags[2],
+    "p",  &eye_flags[3],
+    "s",  &eye_flags[4],
+    "t",  &eye_flags[5],
+    "w",  &eye_flags[6],
+    "y",  &eye_flags[7],
+    "e",  &eye
+  );
+
+  if (help)
+    wordwrap = true;
+
+  for(int i = 0; i < eye_flags.length; i++)
+    if (eye_flags[i])
+      eye = eyes[i];
+
+  auto output = new Message(wordwrap, wrapcolumn);
+  ulong max_length = 0;
+  if (help)
+  {
+    output.add(usage[0]);
+    output.add(usage[1]);
+  }
+  else
+  {
+    if (args.length > 1)
+      output.add(args[1..$].join(' '));
+    else
+    {
+      string line;
+      while ((line = stdin.readln()) !is null)
+        output.add(line.chomp);
+    }
+  }
+
+  output.print();
+
+  writeln(dman.tr("t", thought).tr("e", eye));
+}
+
